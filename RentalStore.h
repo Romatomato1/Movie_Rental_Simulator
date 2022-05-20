2dc7a8c29041e132f97074011a16996e60a09d72
Roman Gofman <roman.p.gofman@gmail.com>
2022-05-20 10:38:40 -0700

    Update RentalStore.h
    Added constructor and comment for it.

--- RentalStore.h
+++ RentalStore.h
@@ -4,8 +4,9 @@
 // 5/20/2022
 // 5/20/2022 
 // --------------------------------------------------------------------------------------------------------------------
-// Rental store reads in all the data from three files in a set order. It then goes on to store said data in 
-// data sets. It contains methods that will process transactions as they come out of the queue and record data.    
+// vThis is the class that will represent our movie rental store. As such, it will include the catalog of movies through
+// a Movie object, a HashTable of Customer objects to store who has an account at the rental store, and a queue of strings
+// to represent transactions done at the rental store.  
 // -------------------------------------------------------------------------------------------------------------------- 
 // Assumes that files are entered in the correct order.
 // --------------------------------------------------------------------------------------------------------------------
@@ -16,6 +17,16 @@
 using namespace std;
 class RentalStore {
 public:
+    //------------------------------------RentalStore-----------------------------------------------------
+    //The constructor will call a readfile helper method for each file that is given in a strict order of 
+    //customer file, transaction file, and movie file. This will also initialize the variables for the 
+    //Movie object, HashTable of Customers, and the queue of strings for transactions. We will call to read
+    //customers first to fill our customer base, then call to read movies to fill the movie catalog, and 
+    //finally call to read commands to make transactions with the data that is now being held. 
+    //@param - 3 string files that represent the customer file, transaction file, and movie file in that strict order. 
+    //@return - No direct return from a constructor. 
+    // ---------------------------------------------------------------------------------------------------
+    RentalStore();
 private:
     struct Customer {
         string firstName;
