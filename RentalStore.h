b67324094d28cf92d66e4939753cfcf529a7c80e
Roman Gofman <roman.p.gofman@gmail.com>
2022-05-20 09:00:53 -0700

    Update RentalStore.h
    5/20/2022
    Changed class description

--- RentalStore.h
+++ RentalStore.h
@@ -1,6 +1,14 @@
-//
-// Created by roman on 5/18/2022.
-//
+// ------------------------------------------------ RentalStore.h -------------------------------------------------------
+- 
+// Krishna Langille, Jacob Tea, Roman Gofman CSS 343 Section B 
+// 5/20/2022
+// 5/20/2022 
+// --------------------------------------------------------------------------------------------------------------------
+// Rental store reads in all the data from three files in a set order. It then goes on to store said data in 
+// data sets. It contains methods that will process transactions as they come out of the queue and record data.    
+// -------------------------------------------------------------------------------------------------------------------- 
+// Assumes that files are entered in the correct order.
+// --------------------------------------------------------------------------------------------------------------------
 
 #ifndef ASSIGNMENT_4_RENTALSTORE_H
 #define ASSIGNMENT_4_RENTALSTORE_H
