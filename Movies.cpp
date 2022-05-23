a82d1a5882e4d9e43e9dadf9997028783e3a8cee
Roman Gofman <roman.p.gofman@gmail.com>
2022-05-23 08:37:48 -0700

    Add files via upload
    Added Movies class

---
+++ Movies.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roman on 5/23/2022.
+//
+
+#include "Movies.h"
---
+++ Movies.h
@@ -0,0 +1,14 @@
+//
+// Created by roman on 5/23/2022.
+//
+
+#ifndef ASSIGNMENT_4_MOVIES_H
+#define ASSIGNMENT_4_MOVIES_H
+
+
+class Movies {
+
+};
+
+
+#endif //ASSIGNMENT_4_MOVIES_H
