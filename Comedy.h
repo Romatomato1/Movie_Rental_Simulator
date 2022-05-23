a1fa24d8da77b3853f746e4473202fefcd0095e8
Roman Gofman <roman.p.gofman@gmail.com>
2022-05-23 09:44:36 -0700

    Update Comedy.h
    Comedy commented so far

--- Comedy.h
+++ Comedy.h
@@ -1,13 +1,32 @@
-//
-// Created by roman on 5/20/2022.
-//
+// ------------------------------------------------ Genre.h -------------------------------------------------------
+- 
+// Krishna Langille, Jacob Tea, Roman Gofman CSS 343 Section B 
+// 5/20/2022
+// 5/23/2022 
+// --------------------------------------------------------------------------------------------------------------------
+// This is the class that will represent our Comedy movie type. It inherits from Genre.
+// -------------------------------------------------------------------------------------------------------------------- 
+// No assumptions.
+// --------------------------------------------------------------------------------------------------------------------
+
 
 #ifndef ASSIGNMENT_4_COMEDY_H
 #define ASSIGNMENT_4_COMEDY_H
 
 
-class Comedy {
-
+class Comedy: public Genre {
+public:
+  //------------------------------------Comedy---------------------------------------------------------
+  //Constructor for comedy class
+  //@param stock - int that says how many copies of a movie that the movie has
+  //@return No return value
+  //----------------------------------------------------------------------------------------------------
+  Comedy(int stock,string director,string title,string releaseYear);
+private:
+  //Class Fields
+  
+  //the year the comedy movie was released
+  int yearReleased;
 };
 
 
