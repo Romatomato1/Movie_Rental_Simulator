0debd54171648215b4b7f3aa5a703a86b9924fb1
Roman Gofman <roman.p.gofman@gmail.com>
2022-05-23 10:35:54 -0700

    Update Customer.h
    Added fields

--- Customer.h
+++ Customer.h
@@ -1,13 +1,28 @@
-//
-// Created by roman on 5/20/2022.
-//
+// ------------------------------------------------ Customer.h -------------------------------------------------------
+- 
+// Krishna Langille, Jacob Tea, Roman Gofman CSS 343 Section B 
+// 5/20/2022
+// 5/23/2022 
+// --------------------------------------------------------------------------------------------------------------------
+// This is the class that will represent our Customer and all the data and transactions associated with a customer.
+// -------------------------------------------------------------------------------------------------------------------- 
+// No assumptions.
+// --------------------------------------------------------------------------------------------------------------------
 
 #ifndef ASSIGNMENT_4_CUSTOMER_H
 #define ASSIGNMENT_4_CUSTOMER_H
 
 
 class Customer {
-
+public:
+private:
+  //Class Fields
+    
+  string firstName;
+  string lastName;
+  int idNumber;
+  string borrowed[];
+  string returned[];
 };
 
 
