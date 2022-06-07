e16c109f18def5e07ca5708921f8302756b5de25
Randgriz11 <88016673+Randgriz11@users.noreply.github.com>
2022-06-07 12:38:42 -0700

    Update Genre.cpp
    VSCODE SUCKS

--- Genre.cpp
+++ Genre.cpp
@@ -1,10 +1,23 @@
-//
-// Created by roman on 5/18/2022.
-//
-//
+// ------------------------------------------------ Genre.cpp -----------------------------------------------------------
+// Krishna Langille, Jacob Tea, Roman Gofman CSS 343 Section B 
+// 5/18/2022
+// 5/24/2022
+// --------------------------------------------------------------------------------------------------------------------
+// This is the class that will represent our movie types and genres. It is the parent class for Comedy,Classics, 
+// and Drama
+// -------------------------------------------------------------------------------------------------------------------- 
+// No assumptions.
+// --------------------------------------------------------------------------------------------------------------------
 #include "Genre.h"
 
-
+//------------------------------------Genre-----------------------------------------------------------
+//This constructor creates a genre of movie. It initializes the director, stock and year released. 
+//The constructor is also inherited from the subclasses.
+//@param stock- int that represents the number of the specific movie that is in inventory
+//@param director- the director of the film
+//@param release year- the release year of the film 
+//@return Has no return
+//----------------------------------------------------------------------------------------------------
 Genre::Genre(int stock,string director, string title){
     char movieType;
     setStock(stock);
@@ -12,14 +25,31 @@ Genre::Genre(int stock,string director, string title){
     setTitle(title);
 }
 
+
+//------------------------------------getStock---------------------------------------------------------
+//Retrieves the int that represents the stock of a movie.
+//@param Has no parameters
+//@return returns an int representing the stock
+//----------------------------------------------------------------------------------------------------
 int Genre::getStock(){
     return stock;
 }
+
+//------------------------------------getDirector---------------------------------------------------------
+//This getterd retrieves the string that represents the director of a movie.
+//@param Has no parameters
+//@return returns a string that represents the director of a movie. 
+//---------------------------------------------------------------------------------------------------- 
 string Genre::getDirector(){
     return director;
 
 }
 
+//------------------------------------getTitle---------------------------------------------------------
+//This getterd retrieves the string that represents the title of a movie.
+//@param Has no parameters
+//@return returns a string that represents the title of a movie. 
+//----------------------------------------------------------------------------------------------------
 string Genre::getTitle(){
     return title;
 }
@@ -56,10 +86,22 @@ char Genre::getMovieType(){
     this->title=title;
 }
 
+//------------------------------------borrowMovie---------------------------------------------------------
+//this decrements the stock of a movie by one
+//@param Has no parameters
+//@return nothing
+//----------------------------------------------------------------------------------------------------
+  void borrowMovie();
 void Genre::borrowMovie() {
     stock--;
 }
 
+//------------------------------------returnMovie---------------------------------------------------------
+//This increments the stock of a movie by 
+//@param Has no parameters
+//@return nothing
+//----------------------------------------------------------------------------------------------------
 void Genre::returnMovie() {
     stock++;
 }
+
